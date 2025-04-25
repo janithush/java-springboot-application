0d4dbd27ff1dcb71b8568ae49d7e943288d459b7
janith ushantha <janith.ushanthaapple@gmail.com>
2025-04-25 12:49:57 +0530

    initial commit

---
+++ src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,60 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String brand;
+    private String madein;
+    private double price;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getMadein() {
+        return madein;
+    }
+
+    public void setMadein(String madein) {
+        this.madein = madein;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Product() {
+    }
+}
